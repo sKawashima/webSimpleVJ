3386bde722d3b5bba4e706510019061e793f94e4
sKawashima <bysk.333@gmail.com>
2019-02-05 10:24:47 +0900

    hello

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 'use strict'
+
+const path = require('path')
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+let mainWindow
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({ width: 600, height: 400 })
+  mainWindow.loadURL(path.join('file://', __dirname, '/index.html'))
+  mainWindow.webContents.openDevTools()
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
