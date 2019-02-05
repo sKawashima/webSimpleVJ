ce60c9c3f4d53f292b09f8f5029466e0404e36fd
sKawashima <bysk.333@gmail.com>
2019-02-05 21:29:31 +0900

    makeChanteTextEffect

--- front/src/index.js
+++ front/src/index.js
@@ -1,13 +1,22 @@
 import './index.styl'
+import ShuffleText from 'shuffle-text'
 
 const ipcRenderer = require('electron').ipcRenderer
+
+const changeText = (params) => {
+  document.getElementById('title').innerHTML = params.title
+  const title = new ShuffleText(document.getElementById('title'))
+  title.start()
+  document.getElementById('artist').innerHTML = params.artist
+  const artist = new ShuffleText(document.getElementById('artist'))
+  artist.start()
+}
 
 const updateViews = (params) => {
   if (params.color) {
     document.getElementById('background').style.backgroundColor = params.color
   }
-  document.getElementById('title').innerHTML = params.title
-  document.getElementById('artist').innerHTML = params.artist
+  changeText(params)
 }
 
 ipcRenderer.on('updateMainView', (ev, params) => {
